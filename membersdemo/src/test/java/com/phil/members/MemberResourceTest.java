620450ed0704f55b0c1ff9068432cffd5b1902b1
Phil Prosser <pprosser@redhat.com>
2020-12-10 16:51:24 +0000

    first commit

---
+++ membersdemo/src/test/java/com/phil/members/MemberResourceTest.java
@@ -0,0 +1,21 @@
+package com.phil.members;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class MemberResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello-resteasy")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy"));
+    }
+
+}
---
+++ membersdemo/src/test/java/com/phil/members/NativeMemberResourceIT.java
@@ -0,0 +1,9 @@
+package com.phil.members;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeMemberResourceIT extends MemberResourceTest {
+
+    // Execute the same tests but in native mode.
+}
