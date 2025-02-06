803f1000a46fa9e7453a37b8eddb889bc1ce0675
Phil Prosser <pprosser@redhat.com>
2025-02-06 11:23:20 +0000

    upgraded

--- membersdemo/src/main/java/com/phil/members/model/Member.java
+++ membersdemo/src/main/java/com/phil/members/model/Member.java
@@ -10,13 +10,13 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import java.util.StringJoiner;
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
